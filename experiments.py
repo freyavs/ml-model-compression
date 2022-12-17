edae65835b751cc3bf88aeef478de7f0f5e4c656
thordossche <dossche.thor@gmail.com>
2022-12-17 12:18:40 +0100

    aa

--- experiments.py
+++ experiments.py
@@ -16,9 +16,7 @@ def teacher_loss_graphic(data:str):
 
 def student_loss_graphic(data:str):
     teacher, _ = kd_loop_teacher(data, load_teacher=True)
-    # _, history = kd_loop_student(data, epochs=30, teacher=teacher)
-    s = get_student_smaller_8_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=30, teacher=teacher)
+    _, history = kd_loop_student(data, epochs=30, teacher=teacher)
     df = pd.DataFrame(history.history)
     df = df.rename(columns={"loss": "train loss", "sparse_categorical_accuracy": "train accuracy", "val_loss": "test loss", "val_sparse_categorical_accuracy": "test accuracy"})
     df.plot().get_figure().savefig('student_loss.png')
@@ -28,44 +26,43 @@ def big_to_small(data:str , teacher_file: str, student_file: str, scratch_file: 
     teacher, history = kd_loop_teacher(data, epochs=20, save=save_accuracy, load_teacher=False)
 
     s = get_student_smaller_cifar10()
-    # _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_2_cifar10()
-    # _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_3_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_4_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
-    return
 
     s = get_student_smaller_5_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_6_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_7_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
     s = get_student_smaller_8_cifar10()
-    _, history = kd_loop_student(data, student=s, epochs=25, teacher=teacher, save=save_accuracy)
-    kd_loop_scratch(data, scratch=s, epochs=25, save=save_accuracy)
+    _, history = kd_loop_student(data, student=s, epochs=13, teacher=teacher, save=save_accuracy)
+    kd_loop_scratch(data, scratch=s, epochs=13, save=save_accuracy)
     # pd.DataFrame(history.history).plot()
 
 
@@ -75,8 +72,8 @@ def normal(data:str , teacher_file: str, student_file: str, scratch_file: str):
     for _ in range(1):
         # TODO: voor grafiekjes is het interessanter om epochs hoger te zetten
         teacher = kd_loop_teacher(data, epochs=1, save=save_accuracy, load_teacher=True)
-        student = kd_loop_student(data, epochs=25, teacher=teacher, save=save_accuracy)
-        scratch = kd_loop_scratch(data, epochs=25, save=save_accuracy)
+        student = kd_loop_student(data, epochs=13, teacher=teacher, save=save_accuracy)
+        scratch = kd_loop_scratch(data, epochs=13, save=save_accuracy)
         compression_result(teacher,student, "teacher_file")
 
 def teacher_pruned(data:str , teacher_file: str, student_file: str, scratch_file: str):
